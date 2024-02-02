f6f8d373a0cb29181992abae3cc0a169ab978cea
vandodev <vandoaparecido@hotmail.com>
2024-02-02 07:55:55 -0300

    :barber: :scissors: :barber: Criando o Search

---
+++ web/app/(home)/_components/search.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Input } from "@/app/_components/ui/input";
+import { Button } from "@/app/_components/ui/button";
+import { SearchIcon, icons } from "lucide-react";
+
+
+const Search = () =>{
+    return(
+     <div className="flex items-center gap-2">
+        <Input placeholder="Busque por uma barbearia..." />
+        <Button variant="default"  size="icon">
+            <SearchIcon size={20} />
+        </Button>
+     </div>
+    )
+}
+
+export default Search;
