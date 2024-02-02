4e0187a2d242da6d0d1180d94160750a59f52ce5
vandodev <vandoaparecido@hotmail.com>
2024-02-02 08:22:42 -0300

    :barber: :scissors: :barber: Componente de agendamento

---
+++ web/app/_components/booking-item.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const BookingItem = () => {
+ return(
+    <Card className="min-w-full">
+        <CardContent className="p-5 flex justify-between py-0">
+            <div className="flex flex-col gap-2 py-5">
+                <Badge  className="bg-[#221c3d] text-primary hover[#221c3d] w-fit">Confirmado</Badge>
+                <h2 className="font-bold">Corte de cabelo</h2>
+                <div className="flex items-center gap-2">
+                    <Avatar className="h-6 w-6">
+                        <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                        <AvatarFallback>A</AvatarFallback>
+                    </Avatar>
+                    <h3 className="text-sm">Vintage Barber</h3>
+                </div>
+
+
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l border-solid px-3 border-secondary">
+                <p className="text-sm">Fevereito</p>
+                <p className="text-2xl">06</p>
+                <p className="text-sm">09:45</p>
+            </div>
+
+        </CardContent>
+    </Card>
+
+ )
+}
+
+export default BookingItem;
