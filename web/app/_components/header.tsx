b01a14287801197fcc011f7dc5493aaa220bed80
vandodev <vandoaparecido@hotmail.com>
2024-02-04 08:31:41 -0300

    :barber: :scissors: :barber: Logando usuário com google

--- web/app/_components/header.tsx
+++ web/app/_components/header.tsx
@@ -5,8 +5,14 @@ import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
+
+const handleLoginClick = async () =>{
+  await signIn();
+}
 
 const Header = () => {
+  const {data} = useSession();
     return(
         <Card>
         <CardContent className="p-5 justify-between items-center flex flex-row">
@@ -14,9 +20,14 @@ const Header = () => {
             <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
           </Link>
 
-          <Button variant="outline" size="icon">
+          {/* <Button variant="outline" size="icon">
             <MenuIcon size={16} />
-          </Button>
+          </Button> */}
+
+          {data?.user 
+            ? <h1>{data.user.name}</h1> 
+            : <Button onClick={handleLoginClick}>Login</Button>
+          }          
        
         </CardContent>
       </Card>
