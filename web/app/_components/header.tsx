9c28c6a6991b3052ceb3c4fc2aaefaa4319c9936
vandodev <vandoaparecido@hotmail.com>
2024-02-06 08:13:54 -0300

    :barber: :scissors: :barber: Mostrando usuário logado

--- web/app/_components/header.tsx
+++ web/app/_components/header.tsx
@@ -3,12 +3,19 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { LogOutIcon, MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle } from "./ui/sheet";
+import { signOut, useSession } from "next-auth/react";
+import { Avatar } from "./ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+
 
 const Header = () => {
-  
+  const {data} = useSession()
+
+  const handleLogoutClick = () => signOut();
+
     return(
         <Card>
         <CardContent className="p-5 justify-between items-center flex flex-row">
@@ -27,7 +34,29 @@ const Header = () => {
               <SheetHeader className="text-left border-b border-solid border-secondary p-5">
                 <SheetTitle>Menu</SheetTitle>
               </SheetHeader>
-             
+              {/* {status === 'authenticated' && ( */}
+              {data?.user ?(
+
+                <div className="flex justify-between px-5 py-6 items-center">
+                  <div className="flex items center gap-3 ">
+                    <Avatar>
+                      <AvatarImage 
+                        src={data.user?.image ?? ""}
+                      />
+                    </Avatar>
+                    <h2 className="font-bold">{data.user.name}</h2>
+                  </div>
+
+                  <Button variant="secondary" size="icon">
+                    <LogOutIcon onClick={handleLogoutClick} />
+                  </Button>
+
+                </div>
+
+              ):(
+                <></>
+              )}
+
             </SheetContent>
           </Sheet>
       
