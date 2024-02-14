b002cd5fad98d50f60d7a86fa8d62d3605900c13
vandodev <vandoaparecido@hotmail.com>
2024-02-14 08:37:28 -0300

    :barber: :scissors: :barber: Mostrando apenas confirmados na tela inicial

--- web/app/(home)/page.tsx
+++ web/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "../_components/header";
-import { format } from "date-fns";
+import { format} from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
@@ -13,9 +13,13 @@ export default async function Home() {
     const session = await getServerSession(authOptions);
     const barbershops = await db.barbershop.findMany({});
 
-    const bookings = session?.user ? await db.booking.findMany({
+    const confirmedBookings = session?.user
+     ? await db.booking.findMany({
         where: {
             userId: (session.user as any).id,
+            date:{
+                gte: new Date()
+            }
         },
         include: {
             service: true,
@@ -43,7 +47,7 @@ export default async function Home() {
         <div className="mt-6 ">
             <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
             <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-                {bookings.map((booking) => (
+                {confirmedBookings.map((booking) => (
                     <BookingItem key={booking.id} booking={booking} />
                 ))}
             </div>          
