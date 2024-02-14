308343e2ae969317cd0a3e6d9fd72f1794d272ad
vandodev <vandoaparecido@hotmail.com>
2024-02-14 08:38:18 -0300

    :barber: :scissors: :barber: Mostrando todos os agendamentos na página inicial

--- web/app/(home)/page.tsx
+++ web/app/(home)/page.tsx
@@ -6,9 +6,23 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
 
 export default async function Home() {
+    const session = await getServerSession(authOptions);
     const barbershops = await db.barbershop.findMany({});
+
+    const bookings = session?.user ? await db.booking.findMany({
+        where: {
+            userId: (session.user as any).id,
+        },
+        include: {
+            service: true,
+            barbershop: true,
+        }
+    }):[]
+
   return (
     <div>
         <Header />
@@ -26,10 +40,14 @@ export default async function Home() {
             <Search />
         </div>
 
-        {/* <div className="px-5 mt-6 ">
-            <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-            <BookingItem />
-        </div> */}
+        <div className="mt-6 ">
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+            <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+                {bookings.map((booking) => (
+                    <BookingItem key={booking.id} booking={booking} />
+                ))}
+            </div>          
+        </div>
 
         <div className="mt-6">
             <h2 className=" px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
