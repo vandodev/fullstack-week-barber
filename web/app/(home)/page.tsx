372745ba5f6a1471a9c6772ab5cecbb48d0e9b6e
vandodev <vandoaparecido@hotmail.com>
2024-02-15 08:36:22 -0300

    :barber: :scissors: :barber: Nome de usúario e boas vindas

--- web/app/(home)/page.tsx
+++ web/app/(home)/page.tsx
@@ -38,13 +38,15 @@ export default async function Home() {
         <Header />
 
         <div className="px-5 pt-5">
-            <h2 className="text-xl font-bold">Olá Evandro</h2>
-            <p className="capitalize text-sm">
-                {format(new Date(), "EEEE',' dd 'de' MMMM", {
-                    locale: ptBR,
-                })}
-            </p>
-        </div>
+        <h2 className="text-xl font-bold">
+          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+        </h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
 
         <div className="px-5 mt-6">
             <Search />
