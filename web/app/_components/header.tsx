72c26a13f8457f25da77001b0b8682af4b6a8216
vandodev <vandoaparecido@hotmail.com>
2024-02-16 08:41:05 -0300

    :barber: :scissors: :barber: Next tags semánticas

--- web/app/_components/header.tsx
+++ web/app/_components/header.tsx
@@ -11,26 +11,28 @@ import SideMenu from "./side-menu";
 const Header = () => {
 
     return(
+      <header>
         <Card>
-        <CardContent className="p-5 justify-between items-center flex flex-row">
-          <Link href="/">
-            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
-          </Link>
+          <CardContent className="p-5 justify-between items-center flex flex-row">
+            <Link href="/">
+              <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+            </Link>
 
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                <MenuIcon size={16} />
-              </Button>
-            </SheetTrigger>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <MenuIcon size={16} />
+                </Button>
+              </SheetTrigger>
 
-            <SheetContent className="p-0">
-              <SideMenu />
-            </SheetContent>
-          </Sheet>      
-       
-        </CardContent>
-      </Card>
+              <SheetContent className="p-0">
+                <SideMenu />
+              </SheetContent>
+            </Sheet>      
+        
+          </CardContent>
+        </Card>
+      </header>
     )
 }
 export default Header;
